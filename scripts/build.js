49ccb3341a2fe2056ef3e14a50ebb3aabdaae539
Daniel Cohen Gindi <danielgindi@gmail.com>
2021-03-30 17:23:10 +0300

    [tools] updated npm dependencies

--- scripts/build.js
+++ scripts/build.js
@@ -65,10 +65,10 @@ const Path = require('path');
         console.info('Generating ' + task.dest + '...');
 
         let plugins = [
-            require('rollup-plugin-node-resolve')({
+            require('@rollup/plugin-node-resolve').nodeResolve({
                 mainFields: ['module', 'main'],
             }),
-            require('rollup-plugin-commonjs')({}),
+            require('@rollup/plugin-commonjs')({}),
         ];
 
         const pkg = require('../package.json');
@@ -80,7 +80,7 @@ const Path = require('path');
         ].join('\n');
 
         if (task.babelTargets) {
-            plugins.push(require('rollup-plugin-babel')({
+            plugins.push(require('@rollup/plugin-babel').babel({
                 sourceMap: !!task.sourceMap,
                 presets: [
                     ['@babel/env', {
@@ -93,6 +93,7 @@ const Path = require('path');
                 minified: false,
                 comments: true,
                 retainLines: true,
+                babelHelpers: 'bundled',
                 exclude: 'node_modules/**/core-js/**/*',
             }));
         }
